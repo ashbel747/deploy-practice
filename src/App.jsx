c2875751ac7849729d0c44cd42863425b15c1af0
Ashbel <kashbel747@gmail.com>
2025-07-12 01:22:24 +0300

    Describe your changes here

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
 import Navbar from './components/Navbar';
+import OurStory from './components/OurStory'
+import SignatureDishes from './components/SignatureDishes';
+import ChatBot from './components/ChatBot';
+import FeedbackForm from './components/FeedbackForm'
+import Footer from './components/Footer';
+import Hero from './components/Hero';
 
 function App() {
+  
   return (
     <div>
       <Navbar />
-      <section id="home" className="p-10 text-center">
-        <h1 className="text-4xl font-bold text-blue-600">Welcome to Tailwind + React</h1>
-      </section>
+      <main>
+        <Hero  />
+        <OurStory  />
+        <ChatBot  />
+        <SignatureDishes />
+        <FeedbackForm  />
+      </main>
+      <footer>
+        <Footer  />
+      </footer>
+      
     </div>
   );
 }
 
 export default App;
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import MenuPage from "./pages/MenuPage";
+import ReservationPage from './pages/ReservationPage';
 import './index.css';
-import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/menu/:type" element={<MenuPage />} />
+        <Route path="/reservation" element={<ReservationPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
